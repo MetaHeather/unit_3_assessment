ae02a779b014972413c59913b97cf3ffff0260c5
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-17 16:04:10 -0500

    fixes delete feature

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.urls import reverse
+
 # Create your models here.
 class Item(models.Model):
   description = models.TextField(max_length=200)
-  def get_absolute_url(self):
-    return reverse('items_index', kwargs={'pk': self.id})
-    # return reverse('items_index')
