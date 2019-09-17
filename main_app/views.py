82e4da624f4947f5407391c305f4160094de1d0e
Heather Etheridge <heathernetheridge@gmail.com>
2019-09-17 13:55:45 -0500

    Adds routes for delete and create

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic import ListView
+from .models import Item
 
-# Create your views here.
+# Define the home view
+class IndexView(ListView):
+    model = Item
+
+
+class ItemCreate(CreateView):
+  model = Item
+  fields = '__all__'
+  def form_valid(self, form):
+    form.save()  
+    return super(ItemCreate, self).form_valid(form) 
+
+class ItemDelete(DeleteView):
+  model = Item
+  success_url = '/'
+
+
+
